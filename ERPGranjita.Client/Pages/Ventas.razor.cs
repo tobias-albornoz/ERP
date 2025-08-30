2b1b6ac1a25107a735e0ece013c1d407e43f230c
tobias-albornoz <tobias020020@gmail.com>
2025-08-30 10:02:41 -0300

    modularizacion

---
+++ ERPGranjita.Client/Pages/Ventas.razor.cs
@@ -0,0 +1,62 @@
+﻿using ERPGranjita.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace ERPGranjita.Client.Pages
+{
+    public partial class Ventas : ComponentBase
+    {
+        [Inject]
+        private HttpClient Http { get; set; } = default!;
+
+        private List<Venta>? ventas;
+        private Venta nuevaVenta = new Venta { Fecha = DateTime.Today };
+        private bool isSaving = false;
+        private string mensaje = "";
+        private string errorMensaje = "";
+
+        protected override async Task OnInitializedAsync()
+        {
+            await CargarVentas();
+        }
+
+        private async Task GuardarVenta()
+        {
+            isSaving = true;
+            mensaje = "";
+            errorMensaje = "";
+            try
+            {
+                var response = await Http.PostAsJsonAsync("api/ventas", nuevaVenta);
+                if (response.IsSuccessStatusCode)
+                {
+                    mensaje = "Venta guardada correctamente";
+                    await CargarVentas();
+                    nuevaVenta = new Venta { Fecha = DateTime.Today };
+                }
+                else
+                {
+                    errorMensaje = "Error al guardar la venta.";
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMensaje = "Error al conectar con la API: " + ex.Message;
+            }
+            isSaving = false;
+        }
+
+        private async Task CargarVentas()
+        {
+            try
+            {
+                ventas = await Http.GetFromJsonAsync<List<Venta>>("api/ventas");
+            }
+            catch
+            {
+                ventas = new List<Venta>();
+                errorMensaje = "Error al cargar ventas desde la API.";
+            }
+        }
+    }
+}
